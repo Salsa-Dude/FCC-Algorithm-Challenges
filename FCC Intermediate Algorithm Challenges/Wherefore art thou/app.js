ad911d0d7e5bf4ff19c681e5237f9dcb808d1da7
Joseph Arias <arias.joseph.a@gmail.com>
2018-05-12 03:00:57 -0400

    filter array

---
+++ FCC Intermediate Algorithm Challenges/Wherefore art thou/app.js
@@ -0,0 +1,18 @@
+
+function whatIsInAName(collection, source) {
+  // Filter collection array
+  return collection.filter(function(obj) {
+    // Loop key in source object
+    for(var i in source) {
+      if(source[i] != obj[i]) {
+        return false
+      }
+    }
+    return true;
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+// whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }) should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }].
+// whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }) should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }].
+// whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "c": 2 }) should return [{ "a": 1, "b": 2, "c": 2 }].
